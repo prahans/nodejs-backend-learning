0f40ca1cc699471435d48b78bed7e18b0002c8a5
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-12 01:42:26 +0545

    learning about fs module

--- 1_node_basics_and_nodejs_core_modules/src/07-fs-module.ts
+++ 1_node_basics_and_nodejs_core_modules/src/07-fs-module.ts
@@ -1,3 +1,16 @@
+import path from "node:path";
+import fs from "node:fs";
+
+const DEMO_FOLDER_PATH = path.join(process.cwd(), "file-system", "fs-demo");
+const SYNC_FILE_PATH = path.join(DEMO_FOLDER_PATH, "sync_note.txt");
+
+type FileResult = {
+  style: string;
+  fileName: string;
+  content: string;
+  sizeInBytes: number;
+};
+
 // fs - file system
 // create folders
 // writte files
@@ -8,3 +21,17 @@
 // sync apis : fs.readfilesync
 // callback apis
 // promise apis
+
+// small startup scripts
+// build scripts
+// local demos
+
+// not good or even bad practice
+// http req handlers
+// high traffic apis
+// background jobs
+
+function runSyncExample(): FileResult {
+  // write content to a file
+  fs.writeFileSync(SYNC_FILE_PATH, "created using fs sync", "utf-8");
+}
