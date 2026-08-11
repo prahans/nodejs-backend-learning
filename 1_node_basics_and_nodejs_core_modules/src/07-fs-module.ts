0b9353daa3ad3fb88e5b25d61f6456ecf41a5d26
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-12 01:23:46 +0545

    learning about fs module

---
+++ 1_node_basics_and_nodejs_core_modules/src/07-fs-module.ts
@@ -0,0 +1,10 @@
+// fs - file system
+// create folders
+// writte files
+// read files
+// check file information
+// delete files
+
+// sync apis : fs.readfilesync
+// callback apis
+// promise apis
