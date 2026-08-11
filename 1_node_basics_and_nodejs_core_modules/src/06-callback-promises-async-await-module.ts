88eb642e5cda469fa07605d48f40e942b7c1d4e7
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-11 22:06:54 +0545

    learning about callback, promise & async

--- 1_node_basics_and_nodejs_core_modules/src/06-callback-promises-async-await-module.ts
+++ 1_node_basics_and_nodejs_core_modules/src/06-callback-promises-async-await-module.ts
@@ -1,3 +1,6 @@
+import { rejects } from "node:assert";
+import { resolve } from "node:dns";
+
 type User = {
   id: number;
   name: string;
@@ -16,7 +19,7 @@ const users: User[] = [
     role: "super-admin",
   },
   {
-    id: 1,
+    id: 3,
     name: "elon",
     role: "user",
   },
@@ -24,3 +27,61 @@ const users: User[] = [
 
 // callback is function - this func u r passing diff function
 // callback(error, result) -> *** imp concept -> classic nodejs callback pattern
+
+// function findUserById(
+//   id: number,
+//   callback: (error: Error | null, user?: User) => void,
+// ) {
+//   const user = users.find((user) => user.id === id);
+//   if (!user) {
+//     callback(new Error("user not found"));
+//     return;
+//   }
+//   callback(null, user);
+// }
+
+// findUserById(1, (error, user) => {
+//   if (error) {
+//     console.error(error.message);
+//     return;
+//   }
+//   console.log(user);
+// });
+
+// function findUserByIdPromise(id: number): Promise<User> {
+//   return new Promise((resolve, rejects) => {
+//     const user = users.find((user) => user.id === id);
+//     if (!user) {
+//       rejects(new Error("user not found"));
+//       return;
+//     }
+
+//     resolve(user);
+//   });
+// }
+
+// findUserByIdPromise(3)
+//   .then((user) => console.log(user))
+//   .catch((error) => console.error(error.message));
+
+async function getUser(id: number): Promise<User> {
+  const user = users.find((user) => user.id === id);
+  if (!user) {
+    throw new Error("user not found");
+  }
+
+  return user;
+}
+
+async function main() {
+  try {
+    const user = await getUser(5);
+    console.log(user);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
+}
+
+main();
