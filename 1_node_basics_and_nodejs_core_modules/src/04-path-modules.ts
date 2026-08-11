fe58f5e82dd1c2c17590ec8b188599deff4bad26
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-11 07:28:22 +0545

    learning about path modules

--- 1_node_basics_and_nodejs_core_modules/src/04-path-modules.ts
+++ 1_node_basics_and_nodejs_core_modules/src/04-path-modules.ts
@@ -8,4 +8,28 @@ import path from "node:path";
 // process.cwd : the folder from where the node js process was started
 
 const projectRoot = process.cwd();
-console.log(projectRoot);
+console.log("projectRoot : ", projectRoot);
+
+// /uploads/users/42/profile.photo.png
+
+const userId = "42";
+const orginalName = "profile.photo.png";
+
+// imp -> path.join -> create a path string
+// it will not create folder
+// it does not check whether the file exists or not
+const uploadFilePath = path.join(
+  projectRoot,
+  "uploads",
+  "users",
+  userId,
+  orginalName,
+);
+
+console.log("uploadFilePath : ", uploadFilePath);
+
+// final part of a path
+const fileName = path.basename(uploadFilePath);
+const fileExt = path.extname(uploadFilePath);
+const parentPath = path.dirname(uploadFilePath);
+console.log(parentPath);
