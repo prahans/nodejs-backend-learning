80e749ce98b7baf596f9b15fb7ec86010f01a9e3
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-11 15:54:22 +0545

    learning about timers module

--- 1_node_basics_and_nodejs_core_modules/src/05-timers-modules.ts
+++ 1_node_basics_and_nodejs_core_modules/src/05-timers-modules.ts
@@ -12,8 +12,26 @@ function runSetTimeoutExample(): void {
   console.log("3. this run immediately. node doesn't wait");
 }
 
-function runTimerDemo() {
-  runSetTimeoutExample();
+function runClearTimeoutExample() {
+  const timerId = setTimeout(() => {
+    console.log("this message will not run");
+  }, 2000);
+  clearTimeout(timerId);
+  console.log("4. cleartimeout cancelled the 2 second timer");
 }
 
-runTimerDemo();
+function runSetIntervalExample() {
+  let count = 0;
+  const intervalId = setInterval(() => {
+    count++;
+    console.log(`setInterval tick ${count}`);
+    if (count === 5) {
+      clearTimeout(intervalId);
+      console.log("setInverval stop");
+    }
+  }, 1000);
+}
+
+// runSetTimeoutExample();
+// runClearTimeoutExample();
+runSetIntervalExample();
