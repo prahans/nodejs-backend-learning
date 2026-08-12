3fb14ac61c8c836e1254a3518ad7000cab08b36b
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-12 10:40:19 +0545

    learning about fs module

--- 1_node_basics_and_nodejs_core_modules/src/07-fs-module.ts
+++ 1_node_basics_and_nodejs_core_modules/src/07-fs-module.ts
@@ -1,8 +1,12 @@
 import path from "node:path";
 import fs from "node:fs";
+import { resolve } from "node:dns";
+import { rejects } from "node:assert";
 
 const DEMO_FOLDER_PATH = path.join(process.cwd(), "file-system", "fs-demo");
 const SYNC_FILE_PATH = path.join(DEMO_FOLDER_PATH, "sync_note.txt");
+const CALLBACK_FILE_PATH = path.join(DEMO_FOLDER_PATH, "callback-note.txt");
+const PROMISE_FILE_PATH = path.join(DEMO_FOLDER_PATH, "promise_note.txt");
 
 type FileResult = {
   style: string;
@@ -52,11 +56,59 @@ function runSyncExample(): FileResult {
   };
 }
 
+function runCallbackExample(): Promise<FileResult> {
+  return new Promise((resolve, rejects) => {
+    fs.writeFile(
+      CALLBACK_FILE_PATH,
+      "create using callback fs",
+      "utf-8",
+      (writeError) => {
+        if (writeError) {
+          rejects(writeError);
+          return;
+        }
+        fs.appendFile(
+          CALLBACK_FILE_PATH,
+          " append using callback fs",
+          "utf-8",
+          (appendError) => {
+            if (appendError) {
+              rejects(appendError);
+              return;
+            }
+            fs.readFile(CALLBACK_FILE_PATH, "utf-8", (readError, content) => {
+              if (readError) {
+                rejects(readError);
+                return;
+              }
+
+              fs.stat(CALLBACK_FILE_PATH, (statError, stats) => {
+                if (statError) {
+                  rejects(statError);
+                  return;
+                }
+
+                resolve({
+                  style: "callback",
+                  content,
+                  sizeInBytes: stats.size,
+                  fileName: path.basename(CALLBACK_FILE_PATH),
+                });
+              });
+            });
+          },
+        );
+      },
+    );
+  });
+}
+
 async function main() {
   try {
     ensureDemoFolderExists();
     const syncResult = runSyncExample();
-    console.log([syncResult]);
+    const callbackResult = await runCallbackExample();
+    console.log([syncResult, callbackResult]);
   } catch (error) {
     if (error instanceof Error) {
       console.error("file system error : ", error.message);
