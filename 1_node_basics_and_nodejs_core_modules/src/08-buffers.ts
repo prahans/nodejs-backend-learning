08f34b6018d41cb5b9923ed004c2107928f7db84
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-13 20:42:49 +0545

    learning about buffer

---
+++ 1_node_basics_and_nodejs_core_modules/src/08-buffers.ts
@@ -0,0 +1,13 @@
+// buffers - raw binary data
+// binary data means - when u have ur data stored in bytes
+// reading files
+// receiving http req bodies
+// working with streams
+// handling images, pdf files, videos
+// encrypt and hashing
+// string - human readable text
+// buffer - raw bytes
+
+const textBuffer = Buffer.from("Node");
+console.log(textBuffer);
+console.log(textBuffer.toString("utf-8"));
