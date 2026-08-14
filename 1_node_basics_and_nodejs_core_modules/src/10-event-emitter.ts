4e0ee1ba50a845b49f319cdda91fd251d78db519
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-14 15:16:00 +0545

    learning about event emitter

--- 1_node_basics_and_nodejs_core_modules/src/10-event-emitter.ts
+++ 1_node_basics_and_nodejs_core_modules/src/10-event-emitter.ts
@@ -353,5 +353,6 @@ for (let i = 0; i < 20; i++) {
 }
 
 // (node:15428) MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 login listeners added to [EventEmitter]. MaxListeners is 10. Use emitter.setMaxListeners() to increase limit
+appEvent.emit("error", new Error("Database failed"));
 appEvent.emit("login", "prahans");
 appEvent.emit("logout");
