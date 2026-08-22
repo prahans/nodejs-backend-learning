43ba0ed5fc0230b1b06b3c6cff265f5865372c98
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-22 17:48:02 +0545

    learning express

--- 3_express/src/servingStaticFile.ts
+++ 3_express/src/servingStaticFile.ts
@@ -1,10 +1,28 @@
 import express, { type Express, type Request, type Response } from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const app: Express = express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.static("public"));
+const user = {
+  name: "prahans",
+  age: 22,
+  city: "birgunj",
+};
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/apple", (req: Request, res: Response) => {
+  res.send("<h1>hello world</h1><h3>apple</h3>");
+});
+
+app.get("/user", (req: Request, res: Response) => {
+  res.json(user);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`app listening on port ${port}`);
 });
