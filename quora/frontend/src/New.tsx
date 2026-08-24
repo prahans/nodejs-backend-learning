907a97cc947604facef616a578636e4706d20ef6
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-24 14:02:48 +0545

    learning backend by building project

--- quora/frontend/src/New.tsx
+++ quora/frontend/src/New.tsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 function New() {
+  const navigate = useNavigate();
   return (
-    <form method="post" action="/posts">
-      <input placeholder="enter username" name="username" type="text" /> <br />{" "}
-      <br />
-      <textarea placeholder="write you post" name="content"></textarea>
-      <br />
-      <button>submit post</button>
-    </form>
+    <>
+      <form method="post" action="/posts">
+        <input placeholder="enter username" name="username" type="text" />{" "}
+        <br /> <br />
+        <textarea placeholder="write you post" name="content"></textarea>
+        <br />
+        <button>submit post</button>
+      </form>
+      <button onClick={() => navigate(-1)}>go back</button>
+    </>
   );
 }
 
