2ac0e63490fb50ff1c294f879e232bd4bde51ac4
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-26 13:10:24 +0545

    learning about middlewares

--- learningAboutmiddlewares/src/index.ts
+++ learningAboutmiddlewares/src/index.ts
@@ -3,8 +3,19 @@ import express, { type Express, type Request, type Response } from "express";
 const app: Express = express();
 const port = 3000;
 
+app.use((req, res) => {
+  const { query } = req.query;
+  console.log(query);
+  console.log("hi i am middleware");
+  res.send("middleware finished");
+});
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Hi, i am root");
+});
+
+app.get("/random", (req: Request, res: Response) => {
+  res.send("Hi, i am random page");
 });
 
 app.listen(port, () => {
