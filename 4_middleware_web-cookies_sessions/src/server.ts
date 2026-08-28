3f8d0cc65677517994c9237a97b2929efb770d74
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-28 17:14:52 +0545

    learning about express-session

--- 4_middleware_web-cookies_sessions/src/server.ts
+++ 4_middleware_web-cookies_sessions/src/server.ts
@@ -6,12 +6,27 @@ import express, {
 } from "express";
 import session from "express-session";
 
+declare module "express-session" {
+  interface SessionData {
+    count: number;
+  }
+}
+
 const app: Express = express();
 const port = 3000;
 
 const secret = "mySuperSecretString";
 
-app.use(session({ secret: secret }));
+app.use(session({ secret: secret, resave: false, saveUninitialized: true }));
+
+app.get("/reqcount", (req: Request, res: Response) => {
+  if (req.session.count) {
+    req.session.count++;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`you send a request ${req.session.count} times`);
+});
 
 app.get("/test", (req: Request, res: Response) => {
   res.send("test successful !");
