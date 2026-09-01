8b074c97a292fdb0245f1b0ebc2a59e7efb21d61
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-01 13:11:14 +0545

    adding userVerification middleware in authRoutes

--- quora/backend/src/routes/authRoutes.ts
+++ quora/backend/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { signup, login } from "../controllers/authController.ts";
+import { userVerification } from "../middlewares/authMiddleware.ts";
 
 const router = Router();
 
+router.post("/", userVerification);
 router.post("/signup", signup);
 router.post("/login", login);
 
