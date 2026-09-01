e705bbca02cd31f51a7bf565e0cfed5bcafbb1e9
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-01 08:31:56 +0545

    adding login function in authRoutes

--- quora/backend/src/routes/authRoutes.ts
+++ quora/backend/src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { signup } from "../controllers/authController.ts";
+import { signup, login } from "../controllers/authController.ts";
 
 const router = Router();
 
 router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
