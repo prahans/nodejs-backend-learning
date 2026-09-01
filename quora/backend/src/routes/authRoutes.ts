45d6274746e60f63e73829f8ba6a3b4124df02f8
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-01 13:20:07 +0545

    removing userVerification middleware from  authRoutes

--- quora/backend/src/routes/authRoutes.ts
+++ quora/backend/src/routes/authRoutes.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
 
 import { signup, login } from "../controllers/authController.ts";
-import { userVerification } from "../middlewares/authMiddleware.ts";
 
 const router = Router();
 
-router.post("/", userVerification);
 router.post("/signup", signup);
 router.post("/login", login);
 
