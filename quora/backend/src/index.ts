b0caa452ad50238b8b936da7c2c9597712042619
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-03 14:11:16 +0545

    improving index.ts

--- quora/backend/src/index.ts
+++ quora/backend/src/index.ts
@@ -1,35 +1,33 @@
 import "dotenv/config";
-import express, { type Express, type Request, type Response } from "express";
+import express, { type Express } from "express";
 import cors from "cors";
-import postRouter from "./routes/postRoutes.ts";
-import authRouter from "./routes/authRoutes.ts";
 import cookieParser from "cookie-parser";
 
+import postRouter from "./routes/postRoutes.ts";
+import authRouter from "./routes/authRoutes.ts";
 import connectDB from "./config/db.ts";
 
 const app: Express = express();
 
 app.use(cookieParser());
 app.use(express.json());
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   }),
 );
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 
 const startServer = async () => {
   try {
-    // 1. Connect to MongoDB first
     await connectDB();
 
-    // 3. Start the server
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
